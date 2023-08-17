297a8595b5543256fb9b28b1f88ac05a191e4609
AnonymouseHucker29 <brent.baylon29@gmail.com>
2023-08-17 23:53:10 +0800

    fix for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Metadata } from "next";
 import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
 import NextAuthSessionProvider from "@/providers/sessionProvider";
 
 export const avgardd = localFont({ src: "../../public/fonts/AVGARDD_2.woff" });
 
-export const metadata = {
+const metadata: Metadata = {
   title: "Blogrr",
   description: "A blog starter built using Next.js",
 };
